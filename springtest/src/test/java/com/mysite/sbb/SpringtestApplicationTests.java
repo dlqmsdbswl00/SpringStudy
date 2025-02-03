eefd39d4cfd94c4d3427cdf58f12e24ae76b16ac
김윤지 <21615008@konyang.ac.kr>
2025-02-03 20:51:30 +0900

    테스트 findall 메서드

--- springtest/src/test/java/com/mysite/sbb/SpringtestApplicationTests.java
+++ springtest/src/test/java/com/mysite/sbb/SpringtestApplicationTests.java
@@ -21,21 +21,21 @@ class SpringtestApplicationTests {
 		
 		List<Question> all = this.questionRepository.findAll();
 		assertEquals(2, all.size());
-		
+		//예상 결과와 실제 결과 동일한지 테스트 asserEquals(기댓값, 실젯값)
 		Question q = all.get(0);
 		assertEquals("sbb가 무엇인가요?", q.getSubject());
 		
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고싶습니다");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);
-		
-		Question q2 = new Question();
-		q2.setSubject("스프링 질문입니다.");
-		q2.setContent("id는 자동 생성 되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);
+//		Question q1 = new Question();
+//		q1.setSubject("sbb가 무엇인가요?");
+//		q1.setContent("sbb에 대해서 알고싶습니다");
+//		q1.setCreateDate(LocalDateTime.now());
+//		this.questionRepository.save(q1);
+//		
+//		Question q2 = new Question();
+//		q2.setSubject("스프링 질문입니다.");
+//		q2.setContent("id는 자동 생성 되나요?");
+//		q2.setCreateDate(LocalDateTime.now());
+//		this.questionRepository.save(q2);
 		
 	}
 
